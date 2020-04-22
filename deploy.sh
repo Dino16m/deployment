31ca296db7892abe519bdd9cc2eaa61df3ad6c75
dino16m <anselem16m@gmail.com>
2020-04-22 14:12:27 +0100

    feat: added mysql feature'

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,8 @@ fi
 
 a2ensite "${APACHEDIR}${APPNAME}.conf":
 systemctl restart apache2
-#CERTBOT CALLS GO HERE
+
+certbot --apache -d $SERVERNAME -d $SERVERALIAS
 
 python3 py_setup.py --servername $SERVERNAME --serveralias $SERVERALIAS --serveradmin $SERVERADMIN --apachedir $APACHEDIR --appname $APPNAME --stack $STACK
 
@@ -46,6 +47,27 @@ if [[ $? == 1 ]]; then
 	exit
 fi
 
+echo what is your mysql root username?
 
+read RootMysqlUser
 
+echo what is your mysql root password?
 
+read MysqlRootPassword
+
+echo input a db name for your app, it will be created.
+
+read DbName
+
+echo input a db user for your app
+
+read DbUser
+
+echo input a db password for user $DbUser
+
+read DbPassword
+
+mysql -u $RootMysqlUser -p $MysqlRootPassword <<EOF
+CREATE DATABASE $DbName DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+GRANT ALL ON ${DbName}.* TO '${DbUser}'@'localhost' IDENTIFIED BY $DbPassword
+EOF
